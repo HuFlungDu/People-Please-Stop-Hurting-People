58bc4a8feb5773c785db20131826534350d21043
Josiah Baldwin <jbaldwin8889@gmail.com>
2013-05-22 15:34:09 -0500

    Added initial async signing support

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,13 +1,19 @@
 class SignaturesController < ApplicationController
   def create
     @signature = @query = Signature.new(params[:signature])
-    @signatures = Signature.limit(50).order("id desc")
-    @signature_count = Signature.count
-    if @signature.save
-        flash[:success] = "Thanks for signing!"
-        redirect_to root_path
-    else
-        render "index"
+    respond_to do |format|
+      if @signature.save
+        @signatures = Signature.limit(50).order("id desc")
+        @signature_count = Signature.count
+        format.html { redirect_to root_path, notice: '' }
+        format.js   {}
+        format.json { render json: @signature, status: :created, location: @signature }
+      else
+        @signatures = Signature.limit(50).order("id desc")
+        @signature_count = Signature.count
+        format.html { render action: "index" }
+        format.json { render json: @signature.errors, status: :unprocessable_entity }
+      end
     end
     
   end
